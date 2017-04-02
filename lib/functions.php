b5a371d9068961d8d0a1f28e5c6b0d8cdb3fdf17
foraern <foraern@gmail.com>
2017-04-02 19:08:17 +0100

    added htaccess and sql file

--- lib/functions.php
+++ lib/functions.php
@@ -6,19 +6,26 @@ date_default_timezone_set('Europe/Dublin');
 define('SALT', 'SALT');
 define('FILESTORAGE', 'db'); //db,file
 
-define('HOST_URL', 'http://' . $_SERVER['SERVER_NAME'] . '/');
-define('DB_HOST', 'localhost');
-define('DB_PORT', 3306);
-define('DB_USER', 'root');
-define('DB_PASSWORD', 'root');
-define('DB_NAME', 'rate-everything');
-define('DATA_DIR', '');
+if ($_SERVER['SERVER_NAME'] == 'rate-everything.app') {//localhost
+    define('HOST_URL', 'http://'.$_SERVER['SERVER_NAME'].'/');
+    define('DB_HOST', 'localhost');
+    define('DB_PORT', 3306);
+    define('DB_USER', 'root');
+    define('DB_PASSWORD', 'root');
+    define('DB_NAME', 'rate-everything');
+    define('DATA_DIR', '');
+} elseif ($_SERVER['SERVER_NAME'] == 'rateeverythingapi-aracnoweb.rhcloud.com') {//openshift
+    define('HOST_URL', 'http://'.$_SERVER['SERVER_NAME'].'/');
+    define('DB_HOST', $OPENSHIFT_MYSQL_DB_HOST);
+    define('DB_PORT', $OPENSHIFT_MYSQL_DB_PORT);
+    define('DB_USER', 'admin39an5fJ');
+    define('DB_PASSWORD', 'Gb_lz673_Z2f');
+    define('DB_NAME', 'rateeverythingapi');
+    define('DATA_DIR', '');
+}
 
-
-include_once('sessions/SessionHandler.php');
+include_once 'sessions/SessionHandler.php';
 
 $session = new MySessionHandler($sessiondb);
 session_set_save_handler($session, true);
 session_start();
-
-?>
